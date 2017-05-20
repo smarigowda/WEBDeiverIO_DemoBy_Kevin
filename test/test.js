eab009bca575a9c4cfc1a6f26ea658258bc964a1
Santosh <santosharakere@gmail.com>
2017-05-20 16:55:52 +0100

    taking screenshot

--- test/test.js
+++ test/test.js
@@ -18,7 +18,7 @@ describe('webdriver.io api page', function() {
     	
     })
 
-    it.only('should take you to the API page', function(){
+    it('should take you to the API page', function(){
     	browser.url('/')
     	browser.click('=API')
     	var title = browser.getTitle()
@@ -26,5 +26,11 @@ describe('webdriver.io api page', function() {
         console.log('pauseing for 10 seconds...')
         // browser.debug()
         browser.pause(10000)
-    }) 
+    })
+
+    it.only('should filter search results', function() {
+        browser.url('/api.html')
+        browser.setValue('input[name="search"]', 'debug')
+        browser.saveScreenshot('api-with-result.png')
+    })
 })
