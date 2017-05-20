6c232448a6148036496ccdf0977256d3b8e12dde
Santosh <santosharakere@gmail.com>
2017-05-20 16:14:15 +0100

    initial

---
+++ test/test.js
@@ -0,0 +1,27 @@
+// var expect = require('chai').expect;
+var assert = require('assert')
+
+describe('webdriver.io api page', function() {
+    it.skip('should have the right title', function() {
+        browser.url('/')
+        var title = browser.getTitle()
+        assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js')
+        // browser.debug()
+    })
+
+    it('should have a link to the API page', function() {
+    	browser.url('/');
+    	// var hasApiLink = browser.isExisting('=APIkjalfjaljf;a');
+    	var hasApiLink = browser.isExisting('=API');
+    	// var hasApiLink = browser.isExisting('a[href="/api.html"]');
+    	assert(hasApiLink);
+    	
+    })
+
+    it.skip('should take you to the API page', function(){
+    	browser.url('/')
+    	browser.click('=API')
+    	var title = browser.getTitle()
+    	assert.equal(title, 'WebdriverIO - API Docs')
+    }) 
+})
